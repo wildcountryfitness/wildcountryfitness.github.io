7a37289b08413f4d512f5a7d19d89a14a2670df5
Rick Clare <rick@wildcountrytech.co.uk>
2016-03-08 17:06:59 +0000

    Adding boostrap-4.0.0-alpha.2

--- js/vendor.js
+++ js/vendor.js
@@ -1,3 +1,18 @@
 ---
 ---
 {% include_relative vendor/jquery.min.js %}
+
+{% comment %}
+  {% include_relative vendor/tether.min.js %}
+  {% include_relative vendor/bootstrap-4.0.0-alpha.2/util.js %}
+  {% include_relative vendor/bootstrap-4.0.0-alpha.2/alert.js %}
+  {% include_relative vendor/bootstrap-4.0.0-alpha.2/button.js %}
+  {% include_relative vendor/bootstrap-4.0.0-alpha.2/carousel.js %}
+  {% include_relative vendor/bootstrap-4.0.0-alpha.2/collapse.js %}
+  {% include_relative vendor/bootstrap-4.0.0-alpha.2/dropdown.js %}
+  {% include_relative vendor/bootstrap-4.0.0-alpha.2/modal.js %}
+  {% include_relative vendor/bootstrap-4.0.0-alpha.2/scrollspy.js %}
+  {% include_relative vendor/bootstrap-4.0.0-alpha.2/tab.js %}
+  {% include_relative vendor/bootstrap-4.0.0-alpha.2/tooltip.js %}
+  {% include_relative vendor/bootstrap-4.0.0-alpha.2/popover.js %}
+{% endcomment %}
